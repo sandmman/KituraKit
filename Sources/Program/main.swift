9a1025d1b8aa4d911aa6d0a0c1bf2eb1edc3fd3f
Ricardo Olivieri <roliv@us.ibm.com>
2017-11-20 17:23:14 -0600

    tests

--- Sources/Program/main.swift
+++ Sources/Program/main.swift
@@ -2,6 +2,7 @@ import KituraKit
 import KituraContracts
 import Dispatch
 
+// A simple codable structure to test with
 public struct Order: Codable {
     public let id: Int
     public let name: String
@@ -11,14 +12,17 @@ public struct Order: Codable {
     }
 }
 
+// Create an instance of Order
 let order = Order(id: 1234, name: "myorder")
 
+// Create KituraKit client
 let client = KituraKit(baseURL: "http://john:pwd1@localhost:8080")!
 print("client: \(client)")
 
 let group = DispatchGroup()
 group.enter()
 
+// Send test request to server (sever must be running locally)
 DispatchQueue.global().async {
     print("Submitting POST to server...")
     client.post("/orders", data: order) { (order: Order?, error: RequestError?) in
@@ -31,5 +35,19 @@ DispatchQueue.global().async {
 // wait ...
 group.wait()
 
+// group.enter()
 
-print("Hello there!")
+// // Send test request to server (sever must be running locally)
+// DispatchQueue.global().async {
+//     print("Submitting POST to server...")
+//     client.post("/orders", data: order) { (authUser: AuthUser, order: Order?, error: RequestError?) in
+//         print("order: \(order!)")
+//         group.leave()
+//     }
+//     print("Completed submitting async request...")
+// }
+
+// // wait ...
+// group.wait()
+
+print("Finished!")
